f2b0e80375d8afb4b3921267bbc460bb56e625b8
cryptobabybear <hello@oliverwee.com>
2020-03-28 18:56:26 +0800

    Done with Part 1

--- part-1/index.js
+++ part-1/index.js
@@ -1,5 +1,24 @@
 const abciServer = require('abci');
+const pino = require('pino');
 
-const server = abciServer({});
+const logger = pino();
+
+const server = abciServer({
+  info: (request) => {
+    return {
+      data: 'Node.Js Todo-App',
+      version: '0.0.0',
+      appVersion: '0.0.0',
+      lastBlockHeight: 0,
+      lastBlockAppHash: Buffer.from(''),
+    };
+  }, 
+  endBlock: (request) => {
+    const { height: rawHeight } = request;
+    const newHeight = Number(rawHeight.toString());
+    logger.info(`Synced height: ${newHeight}`); 
+    return {};
+  },
+});
 
 server.listen(26658);
