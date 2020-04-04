9200274cd18991c0ef562cb26906c77a4b4810eb
cryptobabybear <hello@oliverwee.com>
2020-04-04 10:56:38 +0800

    Saving work on part 2

--- part-2/index.js
+++ part-2/index.js
@@ -3,6 +3,7 @@ const fs = require('fs-extra');
 const pino = require('pino');
 const abciServer = require('abci');
 const stringify = require('json-stable-stringify');
+const TodoApp = require('./todo');
 
 const logger = pino();
 
@@ -11,6 +12,8 @@ const State = () => {
   const mempool = [];
   // Read from file
   const cache = fs.readJsonSync('state.json');
+
+  if (typeof cache.todoList === 'undefined') cache.todoList = [];
 
   const getAppHash = () => {
     if (cache.chainData.lastBlockHeight === 0) return '';
@@ -39,29 +42,44 @@ const State = () => {
 
 const state = State();
 
+const todoApp = TodoApp(state);
+
 const server = abciServer({
-  info: (request) => {
-    return {
-      data: 'Node.Js Todo-App',
-      version: '0.0.0',
-      appVersion: '0.0.0',
-      lastBlockHeight: state.cache.chainData.lastBlockHeight,
-      lastBlockAppHash: Buffer.from(state.getAppHash(), 'hex'),
-    };
-  }, 
+  info: () => ({
+    data: 'Node.Js Todo-App',
+    version: '0.0.0',
+    appVersion: '0.0.0',
+    lastBlockHeight: state.cache.chainData.lastBlockHeight,
+    lastBlockAppHash: Buffer.from(state.getAppHash(), 'hex'),
+  }),
   beginBlock: (request) => {
-    const { header: { height: rawHeight, appHash } } = request;
+    const { header: { height: rawHeight } } = request;
     const nextHeight = Number(rawHeight.toString());
     state.updateChainData('lastBlockHeight', nextHeight);
     return {};
   },
   checkTx: (request) => {
-    const { tx: txBytes } = request;  // Base64
+    const { tx: txBytes } = request; // Base64
+    try {
+      return todoApp.checkTx(txBytes);
+    } catch (err) {
+      console.log(err);
+      return { code: 1, log: 'ABCI App Err' };
+    }
   },
-  commit: (request) => {
+  deliverTx: (request) => {
+    const { tx: txBytes } = request; // Base64
+    try {
+      return todoApp.deliverTx(txBytes);
+    } catch (err) {
+      console.log(err);
+      return { code: 1, log: 'ABCI App Err' };
+    }
+  },
+  commit: () => {
     // Here we persist the state to disk
     const appHash = state.persist();
-    logger.info(`Commited Height: ${state.cache.chainData.lastBlockHeight} AppHash: ${appHash}`); 
+    logger.info(`Commited Height: ${state.cache.chainData.lastBlockHeight} AppHash: ${appHash}`);
     return {
       data: Buffer.from(appHash, 'hex'),
     };
--- part-2/todo.js
+++ part-2/todo.js
@@ -1,14 +1,17 @@
 const crypto = require('crypto');
 const msgpack = require('msgpack');
+const axios = require('axios');
 
+const hashTx = (txBytes) => crypto.createHash('sha256').update(txBytes).digest('hex');
 
 const encodeTx = (tx) => msgpack.pack(tx);
 
 const decodeTx = (txBytes) => {
   try {
-    const txObj = msgpack.unpack(txBytes);
+    const txObj = msgpack.unpack(Buffer.from(txBytes.toString('hex'), 'hex'));
     return txObj;
   } catch (err) {
+    console.log(err);
     return null;
   }
 };
@@ -27,11 +30,11 @@ const addTodoTx = (name) => ({
 
 const add = (state) => ({
   create: (name) => encodeTx(addTodoTx(name)),
-  check: (msgVal) => {
-
-  },
+  check: (msgVal) => typeof msgVal === 'string',
   deliver: (msgVal) => {
-
+    // Mutate the state
+    state.cache.todoList.push(msgVal);
+    return true;
   },
 });
 
@@ -40,11 +43,14 @@ const unpackTx = (txSwitch) => (txOp) => (txObj) => {
   const { type: txType, value: txVal } = txObj;
   if (!isDef(txType) || !isDef(txType)) return { code: 1, log: 'Fail to unpack tx contents' };
   if (txType === 'todo/StdTx') {
-    const { msg } = value;
+    const { msg } = txVal;
     if (!isDef(msg)) return { code: 1, log: 'todo/StdTx is missing parameter msg' };
     const { type: msgType, value: msgVal } = msg;
-    return txSwitch[msgType][txOp](msgVal);
+    const txRes = txSwitch[msgType][txOp](msgVal);
+    if (txOp === 'check' && txRes === true) return { code: 0, log: '' };
+    if (txOp === 'deliver' && txRes === true) return { code: 0, log: '' };
   }
+  return { code: 1, log: `No Handler found for tx ${txType}` };
 };
 
 const txSwitch = {
@@ -52,15 +58,50 @@ const txSwitch = {
 };
 
 const TodoApp = (state) => {
-
   const todoSwitch = Object.keys(txSwitch).reduce(
     (acc, txName) => ({ ...acc, [txName]: txSwitch[txName](state) }),
     {},
   );
 
   const checkTx = (txBytes) => {
+    const txKey = hashTx(txBytes.toString('hex'));
+    if (state.mempool.indexOf(txKey) === -1) {
+      state.mempool.push(txKey);
+      const txObj = decodeTx(txBytes);
+      if (txObj === null) return { code: 1, log: 'Fail to unpack tx with msgpack' };
+      return unpackTx(todoSwitch)('check')(txObj);
+    }
+    return { code: 1, log: 'Tx exists in Mempool' };
+  };
+
+  const deliverTx = (txBytes) => {
+    const txKey = hashTx(txBytes);
+    const memIdx = state.mempool.indexOf(txKey);
+    state.mempool.splice(memIdx, 1);
+    // Remove from mempool
     const txObj = decodeTx(txBytes);
     if (txObj === null) return { code: 1, log: 'Fail to unpack tx with msgpack' };
-    return unpackTx(todoSwitch)('check')(txObj);
+    return unpackTx(todoSwitch)('deliver')(txObj);
+  };
+
+  const broadcastTx = async (msgType, msgVal) => {
+    const stdTx = todoSwitch[msgType].create(msgVal);
+    const txQuery = `0x${stdTx.toString('hex')}`;
+    const queryURL = `http://127.0.0.1:26658/broadcast_tx_async?tx=${txQuery}`;
+    console.log(queryURL);
+    const broadcastResp = await axios({
+      headers: { accept: 'application.json' },
+      method: 'get',
+      url: queryURL,
+    });
+    console.log(broadcastResp.toJSON());
+  };
+
+  return {
+    checkTx,
+    deliverTx,
+    broadcastTx,
   };
 };
+
+module.exports = TodoApp;
