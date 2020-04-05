80fe929950d4a1e104d1b47b4de46701a1d55967
cryptobabybear <hello@oliverwee.com>
2020-04-05 11:25:44 +0800

    Updated test for Echo and Flush

--- test/abci/test_msg_request.js
+++ test/abci/test_msg_request.js
@@ -5,7 +5,11 @@ describe('# ABCI Request Messages', () => {
   describe('# Echo Message', () => {
     it('should encode an echo message', () => {
       const encoded = encode({ msgType: 'echo', msgVal: { message: 'hi', } });
-      assert.equal(encoded, Buffer.from(''));
+      assert.equal(encoded, Buffer.from('0c12040a026869', 'hex'));
+    });
+    it('should encode a flush message', () => {
+      const encoded = encode({ msgType: 'flush', msgVal: {} });
+      assert.equal(encoded, Buffer.from('041a00', 'hex'));
     });
   });
 });
